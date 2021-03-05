e72194486ef590f1aa251b8651624fce35bc385b
Cisco <70318933+C15C01337@users.noreply.github.com>
2021-03-05 19:35:58 +0000

    Add files via upload

---
+++ Find-Leap-Year.py
@@ -0,0 +1,18 @@
+#Devloped By C15C01337 (Bishal Aryal)
+#Follow me on Twitter username @C15C01337
+
+print("Devloped By C15C01337 (Bishal Aryal)")
+print("Follow me on Twitter username @C15C01337")
+
+
+year_input = int(input("Enter the year you want to find leap year:"))  #first ask year as a input from user
+
+def is_leap_year(user_year):    #define a function
+    
+    if user_year % 4 == 0 or user_year % 400 == 0: # use the condition to find a leap year
+        print(f"{user_year} is a leap year")
+    else:
+        print(f"{user_year} is not a leap year")
+    
+is_leap_year(year_input)  # call the function
+
